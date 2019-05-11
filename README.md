8be3d845bb2bd0dd3b9198911242f884770f8369
gerritvanos <gerrit.vanos@student.hu.nl>
2019-05-11 10:23:13 +0200

    first commit
    the first commit to the public of my R2D2 get commit script

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # R2D2-tools
 tools for R2D2 project note: this might not be in line with the R2D2 style guide
+This script is made for personal use to verify or chceck the commits you've made. 
+The use is at own risk and i strongly reccomend checking the links once in a wile.
+
+# Compatibility:
+At this point the "get commits.py" script will only work on windows due to some system calls, this might get fixed later.
+
+
+# Dependencies:
+- atleaste python 3.6 or higher
+- gitpython library 
+
+# How to use it:
+- install gitpython if you don't have it already.
+- Run the script either from commandline `python "get commits.py"`, or via pycharm.
+- when promted type your author name*
+- give a file name to which you want your commit information to be written.
+- specify if you want to get commit information for [wiki,libraries,modules,other, or 1 repo]
+- specify the date from wich you want to get commits in the following format MM-DD-YYYY or type all to get all commits.
+- if you chose 1 repo now specify the repo name.
+- the script will now run and output the information to the screen and the .csv file created in the same folder the script is located.
+- the .csv file can now be opened with excel and all commits will be visible. 
+- **note:** the wiki commits have 2 links, 1 to the compare and 1 to how the page looked at that point. <br>
+The compare links show the progress made in that commit just like a normal repo.
+
+* I've noticed the author name is not always the same, try your username first this usualy gets the most results. <br>
+If you are not sure first type `git log` in a repo you know you've made commits and check your name in the output.
---
+++ get commits.py
@@ -0,0 +1,196 @@
+import git
+import subprocess
+import sys
+import os
+
+modules = ["moving_platform", "location_detector", "end_effectors", "gas_detection", "vein_detection", "sound", "rgb_camera",
+           "swarm_controller", "power", "manual_control", "display", "voice_interaction", "robot_arm", "distance_sensor",
+           "hazard_detection", "microphone", "navigation", "mapping", "swarm_simulation", "communication" , "fire_extinguisher",
+           "health_monitor", "load_sensor", "swarm_managment", "swarm_analytics"]
+
+libraries = ["PWM_library", "USART_library", "internal_communication", "datastructures", "trng_library", "I2C_library"]
+
+others = ["PWM_library","WiringPi", "libusb", "Catch2", "rtos", "hwlib", "r2d2-python-build", "R2D2-build"]
+
+run_path = os.path.dirname(os.path.realpath(__file__))
+
+base_url = "https://github.com/R2D2-2019/"
+
+out_file = None
+
+date_specified = True
+date = None
+
+author_name = ''
+author_command_string = ''
+
+def create_list_from_commits(commits):
+    commits_per_line = commits.split("\n")
+    for commit in range(len(commits_per_line)):
+        commits_per_line[commit] = commits_per_line[commit].split(",")
+    return commits_per_line
+
+def add_url_to_hash(commit_list,repo_url):
+    for commit in range(len(commit_list)):
+        commit_list[commit][0] = str(repo_url + "/commit/" + str(commit_list[commit][0]))
+    return commit_list
+
+def add_wiki_url_to_hash(repo,commit_list):
+    if (len(commit_list) >1):
+        for commit in range(len(commit_list)):
+            file_name = repo.show(commit_list[commit][0],'--name-only', '--pretty=')
+            page_name = file_name[0:-3] #strip .md from file_name
+            prev_commit_hash = 0
+
+            if (":" in file_name):
+                file_name = file_name.replace(":","\:")
+
+            if (len(file_name) is not 0):
+                try:
+                    commits_for_file = create_list_from_commits(repo.log('--no-merges', '--date=iso', '--pretty=%H,%ad,%s', '--follow' , file_name))
+                except:
+                    commits_for_file = []
+                for i in range(len(commits_for_file)):
+                    if commits_for_file[i][0] == commit_list[commit][0]: #found current commit
+                        prev_commit_hash = commits_for_file[i-1][0] #get hash from prev commit
+
+            commit_hash = commit_list[commit][0]
+            commit_list[commit][0] = str(base_url + "R2D2-2019/wiki/" + page_name + "/_compare/" + str(commit_hash)  + '...' + str(prev_commit_hash))
+            commit_list[commit].append(str(base_url + "R2D2-2019/wiki/" + page_name + "/" + str(commit_hash)))
+            
+    return commit_list
+
+def print_2d_list(lst):
+    for i in range(len(lst)):
+        if len(lst[i]) > 1:
+            print(lst[i])
+        else:
+            print("no commits in this repo by " + author_name)
+    print()
+
+def write_list_to_csv(commit_list):
+    global out_file
+    for commit in commit_list:
+        for item in commit:
+            out_file.write(item + ";")
+        out_file.write("\n")
+    out_file.write("\n")
+
+def write_header_to_csv():
+    global out_file
+    out_file.write("link to commit; date; commit message \n")
+
+def write_wiki_header_to_csv():
+    global out_file
+    out_file.write("wiki commits \n")
+    out_file.write("compare link; date; commit message; link to commit \n")
+
+def clone_repo(repo_url,repo_name):
+    try:
+        git.Git("./").clone(repo_url)
+    except:
+        print()
+    return git.Git(run_path + "\\" + repo_name)
+
+def get_commits(repo):
+    if date_specified:
+        return repo.log('--no-merges',author_command_string, '--date=iso', '--pretty=%H,%ad,%s', '--all' , '--since='  + date)
+    return repo.log('--no-merges',author_command_string, '--date=iso', '--pretty=%H,%ad,%s', '--all')
+
+def delete_repo(repo,repo_name):
+    del repo
+    command = "rmdir /S /Q " '"'+ run_path + "\\" + repo_name + '"'
+    subprocess.check_output(command,shell=True)
+
+def get_repo_info(repo_url,repo_name):
+    repo = clone_repo(repo_url,repo_name)
+    complete_list = add_url_to_hash(create_list_from_commits(get_commits(repo)),repo_url)
+    print(repo_name + "\t total commits: " , len(complete_list))
+    print_2d_list(complete_list)
+    out_file.write(repo_name, ", total commits: ", len(complete_list) + '\n')
+    write_header_to_csv()
+    write_list_to_csv(complete_list)
+    delete_repo(repo,repo_name)
+
+def get_all_repos():
+    get_modules()
+    get_libraries()
+    get_others()
+    get_wiki()
+
+def get_modules():
+    print("Modules:")
+    for i in range(len(modules)):
+        repo_name = modules[i]
+        repo_url = base_url + repo_name
+        get_repo_info(repo_url,repo_name)
+
+def get_libraries():
+    print("libraries:")
+    for i in range(len(libraries)):
+        repo_name = libraries[i]
+        repo_url = base_url + repo_name
+        get_repo_info(repo_url,repo_name)
+
+def get_wiki():
+    print ("this might take a while:")
+    wiki_name = "R2D2-2019.wiki"
+    wiki_url = base_url + wiki_name
+    repo = clone_repo(wiki_url, wiki_name)
+
+    complete_list = add_wiki_url_to_hash(repo,create_list_from_commits(get_commits(repo)))
+    print("wiki: \t total commits:" , len(complete_list) )
+    write_wiki_header_to_csv()
+    out_file.write("total commits " +  str(len(complete_list)) + "\n")
+    print_2d_list(complete_list)
+    write_list_to_csv(complete_list)
+    delete_repo(repo,wiki_name)
+
+def get_others():
+    print("others:")
+    for i in range(len(others)):
+        repo_name = others[i]
+        repo_url = base_url + repo_name
+        get_repo_info(repo_url,repo_name)
+
+
+def UI():
+    global author_name
+    global author_command_string
+    global date_specified
+    global date
+    global out_file
+    author_name = input("give a author name please: ")
+    author_command_string = "--author=" + author_name
+
+    file_name = input("please specify a file name(without extention): ")
+    choice = input("get modules,wiki,others,libraries or all or type [one] to get only one repo: ")
+    date = input("specify the date[MM-DD-YYYY] from wich you want to get commits or type [all]: ")
+
+    out_file = open(file_name+".csv", "w")
+    out_file.write("sep=; \n")
+    if (date == "all") :
+        date_specified = False
+    elif (len(date) != 10) :
+        print("invalid date try again ")
+        UI()
+
+    if (choice == "modules"):
+        get_modules()
+    elif (choice == "wiki"):
+        get_wiki()
+    elif (choice == "libraries"):
+        get_libraries()
+    elif (choice == "others"):
+        get_others()
+    elif (choice == "all"):
+        get_all_repos()
+    elif (choice == "one"):
+        repo_name = input("please specify repo_name: ")
+        repo_url = base_url + repo_name
+        get_repo_info(repo_url,repo_name)
+    else:
+        print("not a valid option try again")
+        UI()
+    print()
+UI()
